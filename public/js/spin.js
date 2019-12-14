1caff418229280a7124886c40985aa75262da7c5
MathLah <math.lahousse@gmail.com>
2019-12-14 22:04:13 +0100

    Correction cube

--- public/js/spin.js
+++ public/js/spin.js
@@ -7,13 +7,25 @@
 
     t("body").scrollspy({target: "#sideNav"});
 
-    t('div.cube .front').on('click', function () {
+    t('div.cube.large .front').on('click', function () {
+        let cube = t(this).parent('.cube');
+        cube.addClass('spin-large');
+        cube.removeClass('spin-off-large');
+    });
+
+    t('div.cube.large .right').on('click', function () {
+        let cube = t(this).parent('.cube');
+        cube.addClass('spin-off-large');
+        cube.removeClass('spin-large');
+    });
+
+    t('div.cube.medium .front').on('click', function () {
         let cube = t(this).parent('.cube');
         cube.addClass('spin');
         cube.removeClass('spin-off');
     });
 
-    t('div.cube .right').on('click', function () {
+    t('div.cube.medium .right').on('click', function () {
         let cube = t(this).parent('.cube');
         cube.addClass('spin-off');
         cube.removeClass('spin');
